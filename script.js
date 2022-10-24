cef8f05441f3720b7d0a8f0d94bda243a505bfdb
Foxlove <116038981+jadegfox@users.noreply.github.com>
2022-10-24 23:26:32 +0000

    i got to 8

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+let ageButton = document.querySelector(".age-button")
 
+// task 1:  declare a variable called inputName and assign it to the class called "name" using querySelector.
+let inputName = document.querySelector(".name")
+
+// task 2: declare a variable called inputYear and assign it to the class called "year" using querySelector.
+let inputYear= document.querySelector(".year")
+
+// task 3: declare a variable called message and assign it to the class called "message" using querySelector.
+let message =document.querySelector(".message")
+
+
+
+// task 4: declare a variable called currentYear and set it equal to the current year.
+let currentYear = 2022;
+
+
+// task 5: declare a variable called userName. Do not store a value yet.
+let userName =  inputName;
+
+
+// task 6: declare a variable called userYear. Do not store a value yet.
+let userYear =
+
+
+// task 7: declare a variable called age. Do not store a value yet.
+
+ let age =
+
+
+
+ageButton.onclick = function() {
+
+  // task 8: assign a value to userName. Set it equal to the value of inputName.
+ 
+
+  // task 9: assign a value to userYear. Set it equal to the value of inputYear. This is a number! Be sure to use the Number function on it!
+ 
+
+  // task 10: assign a value to age. We need to calculate this value by subtracting the userYear from currentYear
+ 
+
+  // task 11: using .innerHTML, update message to display the userName and age. Use string interpolation for this to have a personalized message like: ________, it looks like you are (turning) _____ years old!
+
+  
+}
