09ea493d9426769ad4d2bb500ec45b67ad4a3df3
Foxlove <116038981+jadegfox@users.noreply.github.com>
2022-10-31 22:16:52 +0000

    I finished it

--- script.js
+++ script.js
@@ -16,31 +16,33 @@ let currentYear = 2022;
 
 
 // task 5: declare a variable called userName. Do not store a value yet.
-let userName =  inputName;
+let userName 
 
 
 // task 6: declare a variable called userYear. Do not store a value yet.
-let userYear =
+let userYear 
 
 
 // task 7: declare a variable called age. Do not store a value yet.
 
- let age =
+ let age 
 
 
 
 ageButton.onclick = function() {
 
-  // task 8: assign a value to userName. Set it equal to the value of inputName.
- 
+  // task 8: assign a value to userName. Set it equal to the value of inputName. 
+  
+  userName =  inputName.value;
 
-  // task 9: assign a value to userYear. Set it equal to the value of inputYear. This is a number! Be sure to use the Number function on it!
- 
+  // task 9: assign a value to userYear. Set it equal to the value of inputYear. This is a number! Be sure to use the Number function on it! 
+
+ userYear = Number(inputYear.value);
 
   // task 10: assign a value to age. We need to calculate this value by subtracting the userYear from currentYear
- 
+ age =  currentYear - userYear;
 
   // task 11: using .innerHTML, update message to display the userName and age. Use string interpolation for this to have a personalized message like: ________, it looks like you are (turning) _____ years old!
-
+message.innerHTML =`${userName},You are ${age}`;
   
 }
