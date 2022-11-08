aaa05b18b4acbd64a6ef24365ca99c54f9162632
Foxlove <116038981+jadegfox@users.noreply.github.com>
2022-11-08 00:25:26 +0000

    to line 36

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+//all selector variables
+let p = document.createElement("p");
+let h1 = document.querySelector(".header");
+let input = document.querySelector(".input");
+let userInput;
+let ghostImg = document.querySelector(".ghost-image");
+let hideButton = document.querySelector(".hide-button");
+let showButton = document.querySelector(".show-button");
+let updateImg = document.querySelector(".update-img-button");
+let messageButton = document.querySelector(".message-button");
+let message = document.querySelector(".message");
+let nameButton = document.querySelector(".name-button");
 
+
+//create an onclick event for the Hide Me button
+hideButton.onclick = (function (){  
+   ghostImg.style.display = "none";
+});
+
+//create an onclick event for the Show Me button
+showButton.onclick = function () {
+   ghostImg.style.display = "block" ;
+}
+
+
+//create an onclick event for the Update Img button
+updateImg.onclick = function () {
+  ghostImg.setAttribute("src","https://img.freepik.com/premium-vector/halloween-cute-ghost-cartoon-hand-drawn-style_42349-144.jpg?w=2000")
+} ;
+
+
+//create an onclick event for the Send Message button
+//update the userInput variable by saving the value of the input
+//use the insertAdjacentHTML method to append the userInput in a <p> tag
+ messageButton.onclick = function (){
+  message.innerHTML(userInput); 
+ };
+
+
+//create an onclick event for the Name Me button
+//update the userInput variable by saving the value of the input
+//use the innerHTML method to update the h1 tag
